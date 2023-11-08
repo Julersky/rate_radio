b146e16f9323d1e9ef9dd92179f992bbad5eaca7
Julersky <eric.eibl@hotmail.fr>
2023-11-08 19:55:45 +0100

    version beta

--- src/components/Gallery2/Gallery2.js
+++ src/components/Gallery2/Gallery2.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import './Gallery2.scss';
+import './VideoPlayer.scss';
 import './Card.scss';
-import Player from '../Player/Player';
+// import Player from '../Player/Player';
 // Plus tard mettre des props pour le lien API et le nombre de videos a montrer et d'autres filtres si besoin
 
 
@@ -19,35 +20,42 @@ const Gallery2 = ({data, itemShown}) => {
 
   const selectedTitle = data.find((item) => item.id.videoId === selectedCard)?.snippet.title;
 
-  console.log(`http://www.youtube.com/embed/${selectedCard}?enablejsapi=1`)
+ 
 
   return (
-    <div className="gallery">
+    <div className='gallery'>
+      <div className="content-section">
       {data.slice(0, itemShown).map((item, i) => (
-        <div key={i} className="gallery-card" onClick={() => {
-          openCard(item.id.videoId);
-          }}>
-        <div className="card-image">
-          <img src={item.imageUrl} alt={item.snippet.title} />
+        <div key = {i} className="card"onClick={() => {openCard(item.id.videoId)}}>
+          <section className='card-visual'>
+            <img className="card-image"src={item.snippet.thumbnails.medium.url} alt={item.snippet.title}/>
+          </section>
+          <section className='card-text'>
+            <h2 className='card-title'>{item.snippet.title}</h2>
+            <p className='card-description'>{item.snippet.title}</p>
+          </section>
         </div>
-        <div className="card-content">
-          <h3 className="card-title">{item.snippet.title}</h3>
-          <p className="card-description">{item.snippet.title}</p>
-        </div>
-      </div>
+           
       ))}
       {selectedCard !== null && (
         <div className="foreground-card">
           <div className="foreground-content">
             <h4>{selectedTitle}</h4>
-            <Player
-            i = {selectedTitle}
-            source = {selectedCard}
-            />               
+            <section>
+              <iframe 
+                width="560" 
+                height="315" 
+                src={`https://www.youtube.com/embed/${selectedCard}?si=YCMDGsqvvV9_8a0G`} 
+                title={selectedTitle} 
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                allowFullScreen>                
+              </iframe>
+            </section>
             <button onClick={closeCard}>Fermer</button>
           </div>
         </div>
       )}
+      </div>
     </div>
   );
 };
